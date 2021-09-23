f8aa3582b0826612eccd6d1d6b87d8f683f0fda6
aline <contato@varejoforte.com>
2021-09-22 21:34:03 -0300

    feat: Adiciona pagina de cadastro de produto

--- resources/views/produtos/create.blade.php
+++ resources/views/produtos/create.blade.php
@@ -3,12 +3,30 @@
 @section('titulo', 'Produtos - Inserir')
 
 @section('conteudo')
-
-    <h1>Página PRODUTO.CREATE</h1>
-
+<div class="container">
+        <div class="card bg-light  mb-3" >
+            <div class="card-body">
+            <h5 class="card-title fs-2">Produto - Cadastrar</h5>
+            </div>
+        </div>
+        <form method="post" action="{{route('produtos.inserir')}}">
+            @csrf 
+            <div class="mb-3">
+                <label for="nome" class="form-label">Nome</label>
+                <input type="text" class="form-control" id="nome" name="nome">
+            </div>
+            <div class="mb-3">
+                <label for="valor" class="form-label">Valor </label>
+                <input type="text" class="form-control" id="valor" name="valor">
+            </div>
+            <div class="mb-3">
+                <label for="descricao" class="form-label">Descrição </label>
+                <textarea class="form-control" id="descricao" name="descricao" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Salvar</button>
+        </form>
+</div>
 @endsection
-
-
 
 
 
